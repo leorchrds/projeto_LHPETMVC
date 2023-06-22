3b3b01185e119490197502b62d84880ef3d0d639
leorchrds <122051228+leorchrds@users.noreply.github.com>
2023-06-22 08:45:46 -0300

    criado o projeto lhpet mvc

---
+++ Models/Cliente.cs
@@ -0,0 +1,20 @@
+namespace LHPet1.Models;
+
+class cliente
+{
+
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Cpf { get; set; }
+    public string? Email { get; set; }
+    public string? Paciente { get; set; }
+
+    public cliente(int id, string nome, string cpf, string email, string paciente)
+    {
+        this.Id = id;
+        this.Nome = nome;
+        this.Cpf = cpf;
+        this.Email = email;
+        this.Paciente = paciente;
+    }
+}
